cf6e6bb9a4d47a526c6ae116114029b1df673b0c
owenbarrera <owenbarrera000@gmail.com>
2017-01-21 19:02:45 -0300

    programa para aprender a sumar

---
+++ programa.js
@@ -0,0 +1,25 @@
+function multiplicacion(){
+  var a = 3;
+  var b = 6;
+  var bart = a * b;
+  console.log("la multiplicacion es: "+bart);
+}
+
+
+function resta(){
+  var a = 3;
+  var b = 6;
+  var bart = a - b;
+  console.log("la resta es: "+bart);
+}
+
+resta();
+multiplicacion();
+
+
+function suma(a,b) {
+	var bart = a + b;
+	console.log("la suma es: "+bart);
+}
+
+suma(6,9);
